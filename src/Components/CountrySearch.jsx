e65aec2fbf04e67edb12c3f15b4f42df48adcd74
SeikhAbubakar5 <seikhabubakar47@gmail.com>
2024-12-17 23:16:41 +0530

    initial commit

--- src/Components/CountrySearch.jsx
+++ src/Components/CountrySearch.jsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "./CountrySearch.css"
-
+import "./CountrySearch.css";
 
 function CountrySearch() {
-    const [country, setCountry] = useState([]); 
-  const [search, setSearch] = useState("");  
+  const [country, setCountry] = useState([]); 
+  const [search, setSearch] = useState(""); 
   const [filter, setFilter] = useState([]); 
+
 
   const fetchData = async () => {
     try {
-      const response = await axios.get(
-        "https://xcountries-backend.azurewebsites.net/all"
-      );
-      setCountry(response.data);
-      setFilter(response.data);
+      const response = await axios.get("https://restcountries.com/v3.1/all");
+      const countryData = response.data.map((item) => ({
+        name: item.name.common,
+        flag: item.flags.png,
+      }));
+      setCountry(countryData);
+      setFilter(countryData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.log("Error fetching data:", error.message);
     }
   };
 
@@ -25,7 +27,7 @@ function CountrySearch() {
   }, []);
 
   const handleSearch = (e) => {
-    const value = e.target.value.toLowerCase(); 
+    const value = e.target.value.toLowerCase();
     setSearch(value);
 
     const filtered = country.filter((elem) =>
@@ -35,7 +37,7 @@ function CountrySearch() {
   };
 
   return (
-    <>
+    <div className="container">
       <div className="search">
         <input
           type="text"
@@ -48,13 +50,13 @@ function CountrySearch() {
       <div className="Country">
         {filter.map((elem, index) => (
           <div className="countryCard" key={index}>
-            <img src={elem.flag} alt={elem.abbr} />
+            <img src={elem.flag} alt={elem.name} />
             <p>{elem.name}</p>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
-export default CountrySearch
+export default CountrySearch;
